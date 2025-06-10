92de95067c856d2cc23ba83ab568394a4353bd14
achmadakif <83754562+AchmadAkif@users.noreply.github.com>
2025-06-10 20:04:23 +0700

    feat(vite): update configuration for Swiper integration

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,18 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    include: ['swiper/react', 'swiper/modules'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['swiper/react', 'swiper/modules'],
+      output: {
+        globals: {
+          'swiper/react': 'Swiper',
+          'swiper/modules': 'SwiperModules',
+        },
+      },
+    },
+  },
 });
